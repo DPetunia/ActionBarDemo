840adb383b60d6d7f130eb9125c24c7a1a753be9
DPetunia <nurirdhina28@gmail.com>
2024-03-06 12:34:36 +0800

    Initial commit

---
+++ app/src/main/java/com/irdhina/actionbardemo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.irdhina.actionbardemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_home) {
+            Toast.makeText(getApplicationContext(), "Home Click", Toast.LENGTH_SHORT).show();
+            return(true);
+        } else if (id == R.id.action_title) {
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
